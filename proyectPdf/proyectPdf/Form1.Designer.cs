157bcf86889575bd3147e88e4fe59e5a8c81fcbe
ricardonieva <ricardonieva22@gmail.com>
2020-05-14 18:31:07 -0300

    detalles finales

--- proyectPdf/proyectPdf/Form1.Designer.cs
+++ proyectPdf/proyectPdf/Form1.Designer.cs
@@ -37,20 +37,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 212);
+            this.button1.Location = new System.Drawing.Point(69, 326);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 58);
+            this.button1.Size = new System.Drawing.Size(352, 89);
             this.button1.TabIndex = 0;
-            this.button1.Text = "GenerarPDF";
+            this.button1.Text = "Generar PDF";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(46, 77);
+            this.button2.Location = new System.Drawing.Point(69, 118);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(235, 61);
+            this.button2.Size = new System.Drawing.Size(352, 94);
             this.button2.TabIndex = 1;
             this.button2.Text = "Abrir Archivo Excel";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,29 +61,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 165);
+            this.label1.Location = new System.Drawing.Point(78, 254);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 20);
             this.label1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 292);
+            this.label2.Location = new System.Drawing.Point(78, 449);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 329);
+            this.ClientSize = new System.Drawing.Size(492, 506);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CONSTANCIA RG";
